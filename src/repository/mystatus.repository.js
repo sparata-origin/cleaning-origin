ca7e7591fd987b376a47e17848bc9cf674bf564e
OHGILHWAN <dhrlfghks01@gmail.com>
2023-01-03 03:09:00 +0900

    refactor : repository customerStatus attributes 주석처리

--- src/repository/mystatus.repository.js
+++ src/repository/mystatus.repository.js
@@ -5,7 +5,7 @@ class StatusRepository {
   customerStatus = async (userId) => {
     const customerStatusData = await Services.findAll({
       where: { customerId: userId,},
-      attributes : ['id','address','homeImage','customerId','createdAt','status']
+      // attributes : ['id','address','homeImage','customerId','createdAt','status']
     });
     return customerStatusData
   };
