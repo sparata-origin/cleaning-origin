06f32da4b3b8539d12cadd7ba8bceaec4df9abef
Park Jin <qkrwls6521@gmail.com>
2023-01-02 16:21:15 +0900

    add: 서비스 리포지토리 추가

---
+++ src/repository/services.repository.js
@@ -0,0 +1,37 @@
+class ServicesRepository {
+  constructor(ServicesModel) {
+    this.servicesModel = ServicesModel;
+  }
+
+  findAll = async () => {
+    const services = await this.servicesModel.findAll();
+    return services;
+  };
+
+  findById = async (id) => {
+    const service = await this.servicesModel.findByPk(id);
+    return service;
+  };
+
+  create = async () => {
+    const created = await this.servicesModel.create();
+    return created;
+  };
+
+  update = async (id, updateData) => {
+    const updated = await this.servicesModel.update(updateData, {
+      where: { id },
+    });
+    await this.servicesModel.save();
+    return updated;
+  };
+
+  destroy = async (id) => {
+    const destroyed = await this.servicesModel.destroy({
+      where: { id },
+    });
+    return destroyed;
+  };
+}
+
+module.exports = ServicesRepository;
