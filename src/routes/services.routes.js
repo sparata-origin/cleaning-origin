4468b9252898e932aa0fe995f94c9759ebe4cf48
Park Jin <qkrwls6521@gmail.com>
2023-01-02 10:05:05 +0900

    fix: ServicesController 클래스를 인스턴스화 안 하고 넣어서 수정

--- src/routes/services.routes.js
+++ src/routes/services.routes.js
@@ -2,21 +2,22 @@ const express = require("express");
 const router = express.Router();
 const ServicesController = require('../controllers/services.controllers');
 const authMiddleware = require('../middleware/auth.middleware');
+const servicesController = new ServicesController()
 
 // 청소 서비스 신청(고객) API
-router.post('/services', authMiddleware, ServicesController.requestServices)
+router.post('/services', authMiddleware, servicesController.requestServices)
 
 // 청소 서비스 수정(고객) API
-router.put('/services/:serviceId', authMiddleware, ServicesController.putServices)
+router.put('/services/:serviceId', authMiddleware, servicesController.putServices)
 
 // 청소 서비스 삭제(고객) API
-router.delete('/services/:serviceId', authMiddleware, ServicesController.deleteServices)
+router.delete('/services/:serviceId', authMiddleware, servicesController.deleteServices)
 
 // 청소 서비스 목록 조회(전체) API
-router.get('/services/lists', ServicesController.getServicesList)
+router.get('/services/lists', servicesController.getServicesList)
 
 // 청소 서비스 상세 조회(전체) API
-router.get('/services/:serviceId', ServicesController.getServicesDetail)
+router.get('/services/:serviceId', servicesController.getServicesDetail)
 
 
 module.exports = router;
