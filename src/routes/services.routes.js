e58fa91be0d14cd0cc2732f7a2d5332a49c207ca
kingseungil <flykim5115@gmail.com>
2023-01-03 22:34:45 +0900

    Feat: 로그인(임시)

--- src/routes/services.routes.js
+++ src/routes/services.routes.js
@@ -8,14 +8,14 @@ const servicesController = new ServicesController();
 const authMiddleware = require('../middleware/auth.middleware');
 
 try {
-    fs.readdirSync('src/uploads');
+    fs.readdirSync('views/uploads');
 } catch (error) {
     console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-    fs.mkdirSync('src/uploads');
+    fs.mkdirSync('views/uploads');
 }
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'src/uploads'); // 파일 저장 경로
+        cb(null, 'views/uploads'); // 파일 저장 경로
     },
     filename: (req, file, cb) => {
         const ext = path.extname(file.originalname);
