a2f228097d4eda1354c412b5a7fdc17451b3b46d
OHGILHWAN <dhrlfghks01@gmail.com>
2023-01-03 13:02:34 +0900

    build : html ejs 기본 셋팅

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 const { sequelize } = require('../sequelize/models');
 const app = express();
 const port = 3000;
 
 const router = require('./routes');
+const render = require('../render');
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
 app.use('/api', router);
+app.use('/', render),
+
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, '../views/ejs'))
+app.use(express.static(path.join(__dirname, '../views'))); //정적파일, 이미지파일
+
 
 sequelize
     .sync({ force: false })
@@ -18,12 +28,6 @@ sequelize
     .catch((err) => {
         console.error(err);
     });
-
-// 쿠키 확인용(임시)
-app.get('/', (req, res) => {
-    console.log('req:cookies:', req.cookies.user);
-    res.send('OK');
-});
 
 app.listen(port, () => {
     console.log(port, '원조집 OPEN');
