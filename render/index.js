0f04393a7588f2d7b847d213b831b13a5d776d86
OHGILHWAN <dhrlfghks01@gmail.com>
2023-01-03 19:44:59 +0900

    docs : 로그인 상태 홈 페이지 문서 작성

--- render/index.js
+++ render/index.js
@@ -5,4 +5,16 @@ router.get('/', (req, res) => {
   res.render('index.ejs')
 })
 
+router.get('/index', (req, res) => {
+  res.render('index.ejs')
+})
+
+router.get('/status', (req, res) => {
+  res.render('status.ejs')
+})
+
+router.get('/login', (req, res) => {
+  res.render('login.ejs')
+})
+
 module.exports = router;
