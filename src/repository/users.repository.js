459986e3990907e7fd96a00011f4fa7ad3cf98e9
OHGILHWAN <dhrlfghks01@gmail.com>
2023-01-04 17:45:35 +0900

    refactor : 회원가입 코드 리팩토링

--- src/repository/users.repository.js
+++ src/repository/users.repository.js
@@ -2,7 +2,6 @@ const { Users } = require("../../sequelize/models");
 
 class UsersRepository {
   userRegister = async (email, password, nickname, phone, point, isBusiness) => {
-    console.log(isBusiness)
     const createUserData = await Users.create({
       email,
       password,
