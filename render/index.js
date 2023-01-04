8caf62af776072eff4079f25ed4846973251e3c4
OHGILHWAN <dhrlfghks01@gmail.com>
2023-01-04 16:38:00 +0900

    feat : 리뷰작성 API 프론트 연동

--- render/index.js
+++ render/index.js
@@ -38,4 +38,14 @@ router.get('/services',logincheck, (req,res) => {
   res.render('servicedetail.ejs', {login : false})
 })
 
+router.get('/business/review', logincheck, (req, res) => {
+  if (res.locals.user){
+    res.render('writereview.ejs', { login : true})
+  } else if (!res.locals.user) {
+    res.render('writereview.ejs', {login : false})
+  } else {
+    res.render('404.ejs')
+  }
+})
+
 module.exports = router;
