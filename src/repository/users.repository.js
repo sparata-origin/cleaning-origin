b241eda8e6b6d6e19279c9f853b5d5d4527c7160
OHGILHWAN <dhrlfghks01@gmail.com>
2023-01-04 17:44:45 +0900

    refactor : 회원가입 코드 리팩토링

--- src/repository/users.repository.js
+++ src/repository/users.repository.js
@@ -1,35 +1,24 @@
-const { Users } = require('../../sequelize/models');
+const { Users } = require("../../sequelize/models");
 
 class UsersRepository {
-    userRegister = async (email, password, nickname, phone, isBusiness) => {
-        if (isBusiness) {
-            const createUserData = await Users.create({
-                email,
-                password,
-                nickname,
-                phone,
-                point: 0,
-                isBusiness,
-            });
-            return createUserData;
-        } else {
-            const createUserData = await Users.create({
-                email,
-                password,
-                nickname,
-                phone,
-                point: 1000000,
-                isBusiness,
-            });
-            return createUserData;
-        }
-    };
+  userRegister = async (email, password, nickname, phone, point, isBusiness) => {
+    console.log(isBusiness)
+    const createUserData = await Users.create({
+      email,
+      password,
+      nickname,
+      phone,
+      point,
+      isBusiness,
+    });
+    return createUserData;
+  };
 
-    existUser = async (email) => {
-        const existUser = await Users.findOne({ where: { email } });
+  existUser = async (email) => {
+    const existUser = await Users.findOne({ where: { email } });
 
-        return existUser;
-    };
+    return existUser;
+  };
 }
 
 module.exports = UsersRepository;
