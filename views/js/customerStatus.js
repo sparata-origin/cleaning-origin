cec2551c85fa945d94c054dd94421bc2e8783509
kingseungil <flykim5115@gmail.com>
2023-01-05 14:27:37 +0900

    fix : 오탈자 수정

--- views/js/customerStatus.js
+++ views/js/customerStatus.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
-  showStatusList();
-})
+    showStatusList();
+});
 
 function showStatusList() {
-  $.ajax({
-    type: 'GET',
-    url: `/api//mystatus/services`,
-    data: {},
-    success: function (response) {
-      let rows = response.myStatusData
-      console.log(response)
-      for(let i = 0; i < rows.length; i++){
-        let id = rows[i]['id']
-        let status = rows[i]['status']
-        let address = rows[i]['address']
-        let homeImage = rows[i]['homeImage']
-        let businessId = rows[i]['businessId']
+    $.ajax({
+        type: 'GET',
+        url: `/api//mystatus/services`,
+        data: {},
+        success: function (response) {
+            let rows = response.myStatusData;
+            console.log(response);
+            for (let i = 0; i < rows.length; i++) {
+                let id = rows[i]['id'];
+                let status = rows[i]['status'];
+                let address = rows[i]['address'];
+                let homeImage = rows[i]['homeImage'];
+                let businessId = rows[i]['businessId'];
 
-        if (status === "대기중" || status === "청소중") {
-          let temp_html = `<tr>
+                if (status === '대기중' || status === '청소중') {
+                    let temp_html = `<tr>
                           <td class="cart_product">
                             <a href="/services?id=${id}"><img src="/uploads/${homeImage}" alt="" style="width:100px; height:100px;"></a>
                           </td>
                           <td class="cart_description">
-                            <h4>href="/services?id=${id}">${address}</a></h4>
+                            <h4><a href="/services?id=${id}">${address}</a></h4>
                           </td>
                           <td class="cart_price">
                             <p>200000 Point</p>
@@ -35,16 +35,16 @@ function showStatusList() {
                           </td>
                           <td class="cart_total">
                           </td>
-                        </tr>`
-           $('#statusList').append(temp_html)
-        }
-        if (status === "청소완료") {
-          let temp_html = `<tr>
+                        </tr>`;
+                    $('#statusList').append(temp_html);
+                }
+                if (status === '청소완료') {
+                    let temp_html = `<tr>
                           <td class="cart_product">
                           <a href="/services?id=${id}"><img src="/uploads/${homeImage}" alt="" style="width:100px; height:100px;"></a>
                           </td>
                           <td class="cart_description">
-                            <h4>href="/services?id=${id}">${address}</a></h4>
+                            <h4><a href="/services?id=${id}">${address}</a></h4>
                           </td>
                           <td class="cart_price">
                             <p>200000 Point</p>
@@ -57,12 +57,12 @@ function showStatusList() {
                           <td class="cart_total">
                             <button type="button" class="btn btn-warning" onclick="location.href = '/business/review?serviceId=${id}&businessId=${businessId}'">리뷰 작성</button>
                           </td>
-                        </tr>`
-            $('#statusList').append(temp_html)
-        }
-      }
-    },
-  });
+                        </tr>`;
+                    $('#statusList').append(temp_html);
+                }
+            }
+        },
+    });
 }
 
 //onclick="location.href = '/services?id=${id}'"
